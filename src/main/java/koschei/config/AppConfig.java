a6f365a81cc6d1a03ca9bff5fa3485588d2963cd
DaniilKhlestkov <khlestkov00@inbox.ru>
2023-11-23 20:28:18 +0300

    yes

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
-public class AppConfig {
+public class  AppConfig {
 
     @Bean
     public static Island2 getIsland(Wood3 wood) {
